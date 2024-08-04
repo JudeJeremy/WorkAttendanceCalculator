d9d50519df2170bad8bf1ddfb87a58c7a1360bc3
JudeJeremy <86058156+JudeJeremy@users.noreply.github.com>
2024-08-04 09:52:24 +0000

    sds

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -153,7 +153,7 @@ const App = () => {
             Left click to select office days and left click again to unselect
           </h5>
           <h5 className="welcome-subtitle">
-            Pink days are public holidays!
+            Pink days are public holidays
           </h5>
 
           <div className="selected-info">
